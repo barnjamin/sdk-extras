02f14c2e6f1132c9e712dcbc9d6d69836227f940
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-03-30 10:50:35 -0400

    fmt

--- js/verify.ts
+++ js/verify.ts
@@ -1,36 +1,43 @@
-import algosdk, { makePaymentTxn, makePaymentTxnWithSuggestedParamsFromObject, SignedTransaction, SuggestedParams } from 'algosdk'
-import { getAccounts } from './sandbox'
-import nacl from 'tweetnacl'
+import algosdk, {
+  makePaymentTxnWithSuggestedParamsFromObject,
+  SignedTransaction,
+  SuggestedParams,
+} from "algosdk";
+import nacl from "tweetnacl";
 
-function verifySignedTransaction(stxn: SignedTransaction){
+function verifySignedTransaction(stxn: SignedTransaction) {
+  if (stxn.sig === undefined) return false;
 
-    if (stxn.sig === undefined) return false;
+  const pk_bytes = stxn.txn.from.publicKey;
 
-    const pk_bytes = stxn.txn.from.publicKey
+  const sig_bytes = new Uint8Array(stxn.sig);
 
-    const sig_bytes = new Uint8Array(stxn.sig);
+  const txn_bytes = algosdk.encodeObj(stxn.txn.get_obj_for_encoding());
+  const msg_bytes = new Uint8Array(txn_bytes.length + 2);
+  msg_bytes.set(Buffer.from("TX"));
+  msg_bytes.set(txn_bytes, 2);
 
-    const txn_bytes = algosdk.encodeObj(stxn.txn.get_obj_for_encoding())
-    const msg_bytes = new Uint8Array(txn_bytes.length + 2)
-    msg_bytes.set(Buffer.from("TX"))
-    msg_bytes.set(txn_bytes, 2)
-
-    return nacl.sign.detached.verify(msg_bytes, sig_bytes, pk_bytes);
-
+  return nacl.sign.detached.verify(msg_bytes, sig_bytes, pk_bytes);
 }
 
-(function(){
-    const sp: SuggestedParams = {fee: 0, firstRound: 1, lastRound:2, genesisHash:"nDtgfcrOMvfAaxYZSL+gCqA2ot5uBknFJuWE4pVFloo=", genesisID:"sandnet-v1"} 
-    const acct = algosdk.generateAccount()
-    const txn = makePaymentTxnWithSuggestedParamsFromObject({
-        from: acct.addr,
-        to: acct.addr,
-        amount: 0,
-        suggestedParams: sp,
-    })
+(function () {
+  const sp: SuggestedParams = {
+    fee: 0,
+    firstRound: 1,
+    lastRound: 2,
+    genesisHash: "nDtgfcrOMvfAaxYZSL+gCqA2ot5uBknFJuWE4pVFloo=",
+    genesisID: "sandnet-v1",
+  };
+  const acct = algosdk.generateAccount();
+  const txn = makePaymentTxnWithSuggestedParamsFromObject({
+    from: acct.addr,
+    to: acct.addr,
+    amount: 0,
+    suggestedParams: sp,
+  });
 
-    const blob = txn.signTxn(acct.sk)
+  const blob = txn.signTxn(acct.sk);
 
-    const stxn = algosdk.decodeSignedTransaction(blob)
-    console.log("Valid? ", verifySignedTransaction(stxn))
-})()
+  const stxn = algosdk.decodeSignedTransaction(blob);
+  console.log("Valid? ", verifySignedTransaction(stxn));
+})();
